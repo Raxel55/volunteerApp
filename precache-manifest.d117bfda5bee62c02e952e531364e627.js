43d61bd65be99c70732adec9af11fdef4f4b00ad
Arahaon <grey.yurov@yandex.ru>
2019-09-27 21:52:47 +0300

    Updates

---
+++ precache-manifest.d117bfda5bee62c02e952e531364e627.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a03b3a7b6cbad5499e44d11e7e362d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64a5fad82e665072d8fc",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "fd9c301c298c891d6c58",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "64a5fad82e665072d8fc",
+    "url": "/static/js/2.34948b3b.chunk.js"
+  },
+  {
+    "revision": "fd9c301c298c891d6c58",
+    "url": "/static/js/main.068fde63.chunk.js"
+  },
+  {
+    "revision": "20cd925367348f3b5f89",
+    "url": "/static/js/runtime-main.c599de92.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
